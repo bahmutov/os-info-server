60620a03f5e2a8941ed4afebd65f4a96e0b2f3a5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-09-10 10:46:00 -0400

    fix(header): fixed header string

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const msg = 'OS info\n' +
 console.log(msg)
 
 require('http').Server((req, res) => {
-  res.setHeader('Content-Type", "text/plain; charset=utf-8')
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
   res.end(msg)
 }).listen(port)
 console.log('listening on port', port)
