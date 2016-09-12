febd260df84c1acfa5680e8e6379cf1f569cdb1f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-09-12 09:12:05 -0400

    feat(pretty): pretty ms and bytes, closes #1

--- src/index.js
+++ src/index.js
@@ -4,23 +4,31 @@
 
 const port = process.env.PORT || 4000
 const os = require('os')
+const prettyMs = require('pretty-ms')
+const prettyBytes = require('pretty-bytes')
 
-const info = {
-  cpus: os.cpus(),
-  freemem: os.freemem(),
-  loadavg: os.loadavg(),
-  platform: os.platform(),
-  release: os.release(),
-  totalmem: os.totalmem(),
-  type: os.type(),
-  uptime: os.uptime()
+function getStatsMessage () {
+  const info = {
+    cpus: os.cpus(),
+    freemem: prettyBytes(os.freemem()),
+    totalmem: prettyBytes(os.totalmem()),
+    loadavg: os.loadavg(),
+    platform: os.platform(),
+    release: os.release(),
+    type: os.type(),
+    uptime: prettyMs(os.uptime() * 1000)
+  }
+  const msg = 'OS info\n' +
+    JSON.stringify(info, null, 2) + '\n'
+  return msg
 }
-const msg = 'OS info\n' +
-  JSON.stringify(info, null, 2) + '\n'
-console.log(msg)
 
 require('http').Server((req, res) => {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+  const msg = getStatsMessage()
+  console.log(msg)
   res.end(msg)
 }).listen(port)
+
+console.log(getStatsMessage())
 console.log('listening on port', port)
